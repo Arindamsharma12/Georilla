f9aee6fe2b2f66bd5eefca5246d20d9f9532fbb9
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2025-04-10 10:21:34 +0530

    added google api integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Login from './components/Login';
 import Geofence from './components/Geofence';
+import MyMapComponent from './components/MyMap';
 
 function App() {
   const [latitude, setLatitude] = useState('');
@@ -16,24 +17,29 @@ function App() {
         },
         (error) => {
           console.error('Error getting location:', error);
+        },
+        {
+          enableHighAccuracy:true,
+          timeout:10000,
+          maximumAge:0
         }
       );
     }
   }, [loggedIn]);
 
   return (
-    // <div className="App" style={{ textAlign: 'center', color: '#fff', backgroundColor: '#1a1a1a', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    //   {!loggedIn ? (
-    //     <Login onLogin={() => setLoggedIn(true)} />
-    //   ) : (
-    //     <div style={{ padding: '40px', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', backgroundColor: '#1a1a1a' }}>
-    //       <h1>Georilla 🦍</h1>
-    //       <p>Latitude: {latitude}</p>
-    //       <p>Longitude: {longitude}</p>
-    //     </div>
-    //   )}
-    // </div>
-    <Geofence/>
+    <div className="App" style={{ textAlign: 'center', color: '#fff', backgroundColor: '#1a1a1a', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {!loggedIn ? (
+        <Login onLogin={() => setLoggedIn(true)} />
+      ) : (
+        <div style={{ padding: '40px', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', backgroundColor: '#1a1a1a' }}>
+          <h1>Georilla 🦍</h1>
+          <p>Latitude: {latitude}</p>
+          <p>Longitude: {longitude}</p>
+        </div>
+      )}
+    </div>
+    // <Geofence/>
   );
 }
 
